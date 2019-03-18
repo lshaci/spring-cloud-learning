c1ab98ce9234a1befd0f16b623f8e2b14ed963c5
lshaci <lshaci@qq.com>
2019-03-18 20:23:57 +0800

    添加pom描述

--- gateway/src/main/java/com/lshaci/config/GlobalFilterConfig.java
+++ gateway/src/main/java/com/lshaci/config/GlobalFilterConfig.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 
 import java.net.URI;
+import java.util.List;
 
 @Configuration
 public class GlobalFilterConfig {
@@ -16,6 +18,9 @@ public class GlobalFilterConfig {
         return ((exchange, chain) -> {
             System.err.println("==========>测试一下过滤器");
             URI uri = exchange.getRequest().getURI();
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            List<String> authentication = headers.get("authentication");
+            System.err.println(authentication);
             System.err.println(uri.getPath());
             return chain.filter(exchange);
         });
